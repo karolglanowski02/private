d0615bc4c374053752db373a6e4cb1ee84d48998
karolglanowski02 <147157307+karolglanowski02@users.noreply.github.com>
2024-01-05 12:04:51 +0100

    Add files via upload

---
+++ 6 zadanie 1/6 zadanie 1/6 zadanie 1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+//rosnaco
+
+void babelkowe(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+//porownuje elementy oddalone o n - gap/shrink
+void grzebieniowe(int arr[], int n) {
+    int gap = n;
+    float shrink = 1.3;
+    bool swapped;
+
+    do {
+        gap = static_cast<int>(gap / shrink);
+        if (gap < 1) {
+            gap = 1;
+        }
+
+        swapped = false;
+
+        for (int i = 0; i < n - gap; ++i) {
+            if (arr[i] > arr[i + gap]) {
+                std::swap(arr[i], arr[i + gap]);
+                swapped = true;
+            }
+        }
+    } while (gap > 1 || swapped);
+}
+
+void selekcja(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        std::swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    return 0;
+}
---
+++ 6 zadanie 2/6 zadanie 2/6 zadanie 2.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+
+// 1
+
+void merge(int arr[], int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    int* leftArray = new int[n1];
+    int* rightArray = new int[n2];
+
+    for (int i = 0; i < n1; ++i)
+        leftArray[i] = arr[left + i];
+    for (int j = 0; j < n2; ++j)
+        rightArray[j] = arr[middle + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k] = leftArray[i];
+            ++i;
+        }
+        else {
+            arr[k] = rightArray[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArray[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArray[j];
+        ++j;
+        ++k;
+    }
+
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        merge(arr, left, middle, right);
+    }
+}
+
+// 2
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; --i)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; --i) {
+        std::swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+// 3
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; //najlepiej zawsze najwiekszy element
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; ++j) {
+        if (arr[j] < pivot) {
+            ++i;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    return 0;
+}
